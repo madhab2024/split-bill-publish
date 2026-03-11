a8d3eda9378e50aece2fec3c202ca3ef77ccdb9e
Madhab2024 <mondalmadhab4231@gmail.com>
2026-03-12 00:35:29 +0530

    3rd check

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -23,7 +23,7 @@ export default function Home() {
             <section className="hero">
                 <div className="hero-badge">
                     <Sparkles size={16} strokeWidth={2} />
-                    Free Android App madhab.
+                    Free Android App madhab mondal.
                 </div>
                 <h1>Split Bills <br/>Not Friendships</h1>
                 <p>
